74c3408e6aac9274efbbf16284cb776a1f8a80e4
Robert Damaceno Santos <robertdamacenobob@gmail.com>
2023-09-02 21:21:46 -0300

    feat(main): criação de entrypoint para rodar nossa API http

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.DEFAULT_HTTP_PORT;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.listen(PORT);
+}
+
+bootstrap();
